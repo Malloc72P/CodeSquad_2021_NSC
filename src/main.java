7d6d1e66ce20b3d5a1adacfdde36d1d5e49426ee
malloc72p <dase1102@gmail.com>
2020-12-08 20:25:16 +0900

    메인클래스를 따로 만드세요
    * main.java : main함수가 여기에 정의되었습니다. 앞으로 풀게 될 스텝2, 3의 함수는 여기서 호출합니다.
    * Problem1_Word_Pusher.java :
    *** main함수가 제거되었습니다. 대신 runProblem()이 작성되었습니다.(기존 main함수 안에있던 코드가 여기로 옮겨짐)
    *** 변수 이름지을 때, _ 대신 카멜표기법으로 수정했습니다.

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import Solution.Problem1_Word_Pusher;
+
+public class main {
+    public static void main(String[] args){
+        Problem1_Word_Pusher problem1 = new Problem1_Word_Pusher();
+        problem1.runProblem();
+    }
+}
