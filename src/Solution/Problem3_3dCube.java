bedda8201aa17155e0a0b4e6a198ee5cb37b4590
malloc72p <dase1102@gmail.com>
2020-12-08 22:03:14 +0900

    3D 큐브를 구현하고 출력하는 기능을 구현하세요
    * Problem3_3dCube : 3차원 큐브 선언(char 3차원 배열)함. 예시처럼 예쁘게 출력하는 메서드 구현함.
    * main.java : step3코드를 실행하도록 수정함

---
+++ src/Solution/Problem3_3dCube.java
@@ -0,0 +1,137 @@
+package Solution;
+
+import java.util.Scanner;
+
+enum CubeSide{
+    U,F,R,B,L,D,
+}
+
+public class Problem3_3dCube implements MyRunnable{
+
+    @Override
+    public void runProblem() {
+        char[][][] m_cube3D ={
+                {//U
+                        {'B', 'B', 'B'},
+                        {'B', 'B', 'B'},
+                        {'B', 'B', 'B'}
+                },
+                {//F
+                        {'W', 'W', 'W'},
+                        {'W', 'W', 'W'},
+                        {'W', 'W', 'W'}
+                },
+                {//R
+                        {'O', 'O', 'O'},
+                        {'O', 'O', 'O'},
+                        {'O', 'O', 'O'}
+                },
+                {//B
+                        {'G', 'G', 'G'},
+                        {'G', 'G', 'G'},
+                        {'G', 'G', 'G'}
+                },
+                {//L
+                        {'Y', 'Y', 'Y'},
+                        {'Y', 'Y', 'Y'},
+                        {'Y', 'Y', 'Y'}
+                },
+                {//D
+                        {'R', 'R', 'R'},
+                        {'R', 'R', 'R'},
+                        {'R', 'R', 'R'}
+                }
+
+        };
+
+        Scanner sc = new Scanner(System.in);
+        String inputedCmd;
+        advPrintCube(m_cube3D);
+        while(true){
+            System.out.print("CUBE> ");
+            inputedCmd = sc.nextLine().toUpperCase();
+            if(inputedCmd.equals("Q")){
+                System.out.println("Bye~");
+                break;
+            }
+
+            //명령 파싱
+            for (int i = 0 ; i < inputedCmd.length(); i++){
+                String currentCmd = Character.toString(inputedCmd.charAt(i));
+                if(i + 1 != inputedCmd.length()){
+                    //다음 문자가 존재한다면,
+                    if(inputedCmd.charAt(i+1) == '\''){//다음문자가 따옴표면, 현재명령에 붙인다
+                        currentCmd += '\'';
+                        i++;//인덱스 한칸 더 전진
+                    }
+                }
+                System.out.println(currentCmd);
+                processCmd(currentCmd, m_cube3D);//현재 명령어 처리
+                advPrintCube(m_cube3D);
+            }
+        }
+    }
+    private void processCmd(String currentCmd, char[][][] m_cube3D){
+        switch (currentCmd){
+            case "U" :
+
+                break;
+            case "U\'" :
+
+                break;
+            case "R" :
+
+                break;
+            case "R\'" :
+
+                break;
+            case "L" :
+
+                break;
+            case "L\'" :
+
+                break;
+            case "B" :
+
+                break;
+            case "B\'" :
+
+                break;
+        }
+
+    }
+
+    private void advPrintCube(char[][][] m_cube3D){
+        for(int i = 0 ; i < 3; i++){
+            System.out.printf("\t\t\t");
+            for(int j = 0 ; j < 3; j++){
+                System.out.print(m_cube3D[CubeSide.U.ordinal()][i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i = 0 ; i < 3; i++){
+            for(int side = 1; side <= 4; side++){
+                for(int j = 0 ; j < 3; j++){
+                    System.out.print(m_cube3D[side][i][j] + " ");
+                }
+                System.out.printf("\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i = 0 ; i < 3; i++){
+            System.out.printf("\t\t\t");
+            for(int j = 0 ; j < 3; j++){
+                System.out.print(m_cube3D[CubeSide.D.ordinal()][i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    private void rotateUpper(char[][] m_cube3D){//U
+
+    }
+    private void rotateUpperReverse(char[][] m_cube3D){//U'
+
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,11 @@
 import Solution.MyRunnable;
 import Solution.Problem1_Word_Pusher;
 import Solution.Problem2_2dCube;
+import Solution.Problem3_3dCube;
 
 public class main {
     public static void main(String[] args){
-        MyRunnable problem = new Problem2_2dCube();
+        MyRunnable problem = new Problem3_3dCube();
         problem.runProblem();
     }
 }
